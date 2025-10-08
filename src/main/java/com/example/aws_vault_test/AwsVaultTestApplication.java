81fc7acef2edcd68cac482f41c3cc798e8a258f1
el-mehdi.slaoui <el-mehdi.slaoui@levio.ca>
2025-10-08 16:14:42 -0400

    update

--- src/main/java/com/example/aws_vault_test/AwsVaultTestApplication.java
+++ src/main/java/com/example/aws_vault_test/AwsVaultTestApplication.java
@@ -1,13 +1,33 @@
 package com.example.aws_vault_test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AwsVaultTestApplication {
+import com.example.aws_vault_test.service.AwsSecretsService;
 
+@SpringBootApplication
+public class AwsVaultTestApplication implements CommandLineRunner {
+
+	@Autowired
+    private AwsSecretsService secretsService;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(AwsVaultTestApplication.class, args);
+		System.out.println("--- STARTING THE APPLICATION ---");
+		SpringApplication application = new SpringApplication(AwsVaultTestApplication.class);
+		application.setWebApplicationType(WebApplicationType.NONE);
+		application.run(args);
+		System.out.println("--- APPLICATION FINISHED ---");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Application context fully initialized!");
+		System.out.println("Getting secret names...");
+		secretsService.getAllSecretNames().forEach(System.out::println);
+
 	}
 
 }
